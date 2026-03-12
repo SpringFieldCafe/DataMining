cecd35bb9cb5f0db8eb362a261f3a9ba638344ca
SpringFieldCafe <2574703489@qq.com>
2026-03-12 08:16:40 +0800

    add all assignments

---
+++ assignment4/作业4-kmeans聚类/cluster.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('D:/Mall_Customers.csv')
+
+# Segmentation using Age and spending Score
+# 1、K-Means中，首先确定K的取值
+# 2、K-Means中，确定起始中心点（提示：K-Means++优化）
+# 3、K-Means中，确定算法的终止条件
+# 4、画出聚类图，同一类的点颜色相同，不同类的点颜色不同
+
+
+# Segmentation using Annual Income and Spending Score
+
+# Segmentation using Age , Annual Income and Spending Score
